48c90fa33476150372bed144cf124ac687fa2154
John Kuras <w7og@yahoo.com>
2026-08-01 15:06:03 -0700

    fixed touch statement in gengitver.sh

--- gengitver.sh
+++ gengitver.sh
@@ -4,5 +4,5 @@ GITHEAD=$(git rev-parse HEAD)
 GITTAG=$(git describe --always --dirty --tags)
 GITBRANCH=$(git branch --show-current)
 echo "const char *gitcommit = \"$GITHEAD\";const char *gittag = \"$GITTAG\";const char *gitbranch = \"$GITBRANCH\";"  > src/gitversion.h
-touch src/findwords.cpp
+touch src/LuckyMaJong.c
 exit
